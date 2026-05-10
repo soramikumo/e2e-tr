10b5e2613e2f8e71dda90922f4d04e8dc740cc25
soramikumo <sorao0324@gmail.com>
2026-05-10 13:15:56 +0900

    docker fileの修正

--- runner/executor/codegen.go
+++ runner/executor/codegen.go
@@ -1,6 +1,7 @@
 package executor
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -16,8 +17,10 @@ func ExecuteCodegen(c *domain.Codegen, testsDir string, cs store.CodegenStore) {
 
 	c.Send(domain.CodegenEvent{Type: "status", Message: "ブラウザを起動しています..."})
 
+	var stderr bytes.Buffer
 	cmd := exec.Command("npx", "playwright", "codegen", "--output", outputFile, c.URL)
 	cmd.Dir = testsDir
+	cmd.Stderr = &stderr
 
 	if err := cmd.Start(); err != nil {
 		c.Finish("", fmt.Errorf("起動失敗: %v", err))
@@ -30,9 +33,9 @@ func ExecuteCodegen(c *domain.Codegen, testsDir string, cs store.CodegenStore) {
 
 	if _, statErr := os.Stat(outputFile); statErr != nil {
 		if err != nil {
-			c.Finish("", fmt.Errorf("記録失敗: %v", err))
+			c.Finish("", fmt.Errorf("記録失敗: %v\n%s", err, stderr.String()))
 		} else {
-			c.Finish("", fmt.Errorf("ファイルが保存されませんでした"))
+			c.Finish("", fmt.Errorf("ファイルが保存されませんでした\n%s", stderr.String()))
 		}
 		return
 	}
