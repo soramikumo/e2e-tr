735a870beacccf09bb4d062cee3c08df21f543d7
soramikumo <sorao0324@gmail.com>
2026-05-21 17:58:00 +0900

    playwrigthのtraceをOnにするように修正

--- tests/playwright.portal.config.ts
+++ tests/playwright.portal.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   reporter: [['html', { open: 'never', outputFolder: 'playwright-report' }], ['line']],
   use: {
     baseURL: process.env.PORTAL_URL ?? 'http://localhost:3000',
-    trace: 'on-first-retry',
+    trace: 'on',
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
   },
